a915b1f60194d24cad3d0ea2cd75388974c8e29d
RenanRodriguesRecife <renan_ime_2012@hotmail.com>
2021-03-07 22:20:16 -0300

    update 2

--- knn.py
+++ knn.py
@@ -1,6 +1,6 @@
 #base de dados escolhidas
 #DATATRIEVE Transition/Software defect prediction
-
+#Desharnais Class-level data for KC1 (Defect Count)/Software defect prediction
 
 #biblioteca para importar formato arff
 from scipy.io import arff
@@ -10,28 +10,89 @@ import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 #biblioteca para fazer o kfold cross-validation
 from sklearn.model_selection import cross_val_score
+#biblioteca para fazer gráficos
+import seaborn as sns
+import matplotlib.pyplot as plt
+#calcular o tempo de execução
+import time
 
 
+'''
 data = arff.loadarff('data1.arff')
 df = pd.DataFrame(data[0])
 #print(df.head())
 
 X = df.drop(columns=['Faulty6_1'])
 print(X)
-Y = df['Faulty6_1'].values.toInt
+
+Y = df['Faulty6_1'] = pd.to_numeric(df['Faulty6_1'],downcast='integer')
+print(Y)
+
+'''
+data = arff.loadarff('data2.arff')
+df = pd.DataFrame(data[0])
+
+X = df.drop(columns=['NUMDEFECTS'])
+print(X)
+
+Y = df['NUMDEFECTS']
 print(Y)
 
 
 
-#KNN normal
-#knn = KNeighborsClassifier(n_neighbors=10,weights='uniform',n_jobs=-1)
+
+print('KNN')
+
+arrayTest = []
+
+for K in [1,2,3,5,7,9,11,13,15]:
+    #KNN normal
+    print ("para K = " + str(K))
+    knn = KNeighborsClassifier(n_neighbors=K,weights='uniform',metric='euclidean',n_jobs=-1)
+    
+    start_time = time.time()
+    score = cross_val_score(knn, X, Y, cv=10,scoring='accuracy')
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+    print(score)
+    print('média: '+ str(score.mean()))
+    arrayTest.append([score.mean(),K])
+
+graf1 = pd.DataFrame(arrayTest,columns=["acurácia média","valor de K"])
 
 
-#KNN Ponderado
-knn = KNeighborsClassifier(n_neighbors=10,weights='distance',n_jobs=-1)
+print(graf1)
+sns.lineplot(data=graf1, palette="tab10", linewidth=2.5)
+plt.ylim(0, 1)
 
 
-score = cross_val_score(knn, X, Y, cv=10,scoring='accuracy')
+plt.show()
 
-print(score)
-print('chegou aqui')
+
+print('KNN PONDERADO')
+
+arrayTest = []
+
+for K in [1,2,3,5,7,9,11,13,15]:
+    
+    #KNN Ponderado
+    print ("para K = " + str(K))
+    
+    knn = KNeighborsClassifier(n_neighbors=K,weights='distance',metric='euclidean',n_jobs=-1)
+    
+    start_time = time.time()
+    score = cross_val_score(knn, X, Y, cv=10,scoring='accuracy')
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+    print(score)
+    print('média: '+ str(score.mean()))
+    arrayTest.append([score.mean(),K])
+
+graf1 = pd.DataFrame(arrayTest,columns=["acurácia média","valor de K"])
+
+print(graf1)
+sns.lineplot(data=graf1, palette="tab10", linewidth=2.5)
+plt.ylim(0, 1)
+
+
+plt.show()
